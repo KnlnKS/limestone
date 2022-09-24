21e2d6666a8537419698aad1ebfcf002486e688d
Kunalan Kevin Subagaran <soulkks@hotmail.ca>
2022-09-24 17:56:45 -0400

    🔬 Choose UI Framework for Project (#7)
    * ➕ Add bootstrap
    
    * 💄 Import bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  /*
+  // Uncomment this if you need to use interactive Bootstrap components
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap');
+  }, []);
+  */
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
