2aa491fbd90c6180b47a99d282da498d51fa6cbd
KnlnKS <soulkks@hotmail.ca>
2022-10-05 22:23:54 -0400

    💄 Add theme

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,21 @@ import "../styles/globals.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
 
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  brand: {
+    100: "#ed7d31",
+    200: "#7f7f7f",
+    300: "#f79646",
+    400: "#984807",
+    500: "#d9d9d9",
+  },
+});
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
