566363e1895fd8602e90681e6e57ff66a925eb0a
KnlnKS <soulkks@hotmail.ca>
2022-10-05 22:13:40 -0400

    ⏪️ Revert "🔬 Choose UI Framework for Project (#7)"

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,7 @@
-import "bootstrap/dist/css/bootstrap.css";
-import "../styles/globals.css";
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  /*
-  // Uncomment this if you need to use interactive Bootstrap components
-  useEffect(() => {
-    import('bootstrap/dist/js/bootstrap');
-  }, []);
-  */
-
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
